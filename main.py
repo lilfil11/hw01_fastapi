756da79bcc26b10274fef2b7b1c788226d226541
lilfil11 <filatovka11@gmail.com>
2023-11-01 20:19:49 +0300

    Add response validation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from enum import Enum
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from time import time
+from typing import List, Dict
 
 app = FastAPI()
 
@@ -42,37 +43,43 @@ post_db = [
 @app.get('/')
 def root():
     # ваш код здесь
-    return f'You are in the root!'
+    return 'You are in the root!'
 
 
 # ваш код здесь
 @app.post('/post')
-def get_post():
+def get_post() -> Timestamp:
     ts = Timestamp(id=len(post_db), timestamp=int(time()))
     post_db.append(ts)
     return ts
 
 
 @app.get('/dog')
-def get_dogs(kind: DogType):
-    return [{'name': dog.name, 'pk': dog.pk, 'kind': dog.kind} for dog in dogs_db.values() if dog.kind == kind]
+def get_dogs(kind: DogType = None) -> List[Dog]:
+    return [dog for dog in dogs_db.values() if dog.kind == kind]
 
 
 @app.post('/dog')
-def create_dog(dog: Dog):
-    dog.pk = len(dogs_db)
+def create_dog(dog: Dog) -> Dog:
+    if dog.pk in dogs_db:
+        raise HTTPException(status_code=409, detail='The specified PK already exists.')
+
     dogs_db[dog.pk] = dog
     return dog
 
 
 @app.get('/dog/{pk}')
-def get_dog_by_pk(pk: int):
-    return {'name': dogs_db[pk].name, 'pk': dogs_db[pk].pk, 'kind': dogs_db[pk].kind}
+def get_dog_by_pk(pk: int) -> Dog:
+    return dogs_db[pk]
 
 
 @app.patch('/dog/{pk}')
-def update_dog(pk: int, dog: Dog):
-    dog.pk = pk
+def update_dog(pk: int, dog: Dog) -> Dog:
+    if pk not in dogs_db:
+        raise HTTPException(status_code=409, detail="Dog with the entered PK doesn't exist.")
+    elif pk != dog.pk:
+        raise HTTPException(status_code=409, detail='The entered PK do not match.')
+
     dogs_db[pk].name = dog.name
     dogs_db[pk].kind = dog.kind
     return dog
